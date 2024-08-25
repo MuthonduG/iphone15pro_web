df6a871c23b2103e2d0a4a87cf78dd6982b1dc27
MuthonduG <muthondugithinji@gmail.com>
2024-08-25 08:59:53 +0300

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import  { appleImg, bagImg, searchImg } from "../utils"
+import { navLists } from "../constants"
+const Navbar = () => {
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="apple logo" height={18} width={14}/>
+        <div className="flex flex-1 justify-center max-sm:hidden">
+            {
+                navLists.map((nav_item)=> {
+                return(
+                    <div key={nav_item} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+                        {nav_item}
+                    </div>
+                )
+            })
+            }
+        </div>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+            <img src={searchImg} alt="search" width={18} height={18}/>
+            <img src={bagImg} alt="bag" width={18} height={18}/>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,10 @@
+
+const VideoCarousel = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default VideoCarousel
